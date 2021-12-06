62629cdbaad5c638c9d17e4a5e0a70621923b481
Geovani Pedroso <geeopedroso@gmail.com>
2021-12-06 10:56:49 -0300

    index.ts

--- TaxonHub/src/Backend/src/index.ts
+++ TaxonHub/src/Backend/src/index.ts
@@ -1,11 +1,11 @@
-// import express from 'express'
+import express from 'express'
 
-// const app = express();
+const app = express();
 
-// app.get('/', (request, response) => {
-//     return response.json({message: 'teste'});
-// });
+app.get('/', (request, response) => {
+    return response.json({message: 'teste'});
+});
 
-// app.listen(3333, () => {
-//     console.log('Aplicação rodandossss em http://localhost:3333');
-// })
+app.listen(3333, () => {
+    console.log('Aplicação rodandossss em http://localhost:3333');
+})

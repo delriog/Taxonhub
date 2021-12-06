9567a30dbdfd96813088878464ab278c5fbc9ce4
Geovani Pedroso <geeopedroso@gmail.com>
2021-12-06 16:40:55 -0300

    alterando projeto base

--- TaxonHub/src/Backend/controllers/home.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-
-module.exports = () => {
-    const controller = {};
-    console.log("hello world!")
-  
-    return controller;
-}
--- TaxonHub/src/Backend/routes/home.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-    const controller = require('../controllers/home')();
-  
-    app.route('/')
-      .get(controller.home);
-}

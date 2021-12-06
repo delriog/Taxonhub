4e4b1dfc56d54cf14dfb06d52d9cf645c3030a99
Geovani Pedroso <geeopedroso@gmail.com>
2021-12-06 15:09:11 -0300

    buscando dados no site

--- TaxonHub/src/Backend/src/index.ts
+++ TaxonHub/src/Backend/src/index.ts
@@ -1,11 +1,35 @@
-import express from 'express'
+import express from "express";
+import axios from "axios";
+import Papa from 'papaparse';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'teste'});
+app.get("/", (request, response) => {
+  axios
+    .get("http://www.theplantlist.org/tpl1.1/search", {
+      params: {
+        q: "Eichhornia azurea",
+        csv: true,
+      },
+      timeout: 10000,
+    })
+    .then((res) => {
+        return response.send(res.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          throw new Error(
+            `Website response error with status: ${error.response.status}`,
+          );
+        } else if (error.request) {
+          throw new Error(`Website request error: ${error.request}`);
+        } else {
+          throw new Error(`Error in setting up the request: ${error.message}`);
+        }
+      });
+
 });
 
 app.listen(3333, () => {
-    console.log('Aplicação rodandossss em http://localhost:3333');
-})
+  console.log("Aplicação rodandossss em http://localhost:3333");
+});

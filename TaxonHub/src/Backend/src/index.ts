57d58121edca70747a00e496753e60d34c7eeca4
unknown <user_vector31@tribanco.com.br>
2021-12-06 10:47:20 -0300

    criação index.ts

---
+++ TaxonHub/src/Backend/src/index.ts
@@ -0,0 +1,11 @@
+// import express from 'express'
+
+// const app = express();
+
+// app.get('/', (request, response) => {
+//     return response.json({message: 'teste'});
+// });
+
+// app.listen(3333, () => {
+//     console.log('Aplicação rodandossss em http://localhost:3333');
+// })

fb0d1b8e68d95eaf6503d87c826c618491196671
Geovani Pedroso <geeopedroso@gmail.com>
2021-12-10 11:44:09 -0300

    adicionando axios

--- TaxonHub/src/Backend/api/controllers/thePlantListController.js
+++ TaxonHub/src/Backend/api/controllers/thePlantListController.js
@@ -1,7 +1,36 @@
+const axios = require("axios");
 module.exports = () => {
-    const controller = {};
-  
-    controller.buscarDados = (req, res) => res.status(200).send("foi carai...");
-  
-    return controller;
-  }
+  const controller = {};
+
+  controller.buscarDados = async (req, res) => {
+    let dados = await buscarEspecies("Eichhornia azurea");
+    res.send(dados);
+  };
+
+  const buscarEspecies = async (nomeEspecie) => {
+    return axios
+      .get("http://www.theplantlist.org/tpl1.1/search", {
+        params: {
+          q: nomeEspecie,
+          csv: true,
+        },
+        timeout: 10000,
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        if (error.response) {
+          throw new Error(
+            `Website retornou com erro: ${error.response.status}`
+          );
+        } else if (error.request) {
+          throw new Error(`Website request error: ${error.request}`);
+        } else {
+          throw new Error(`Error in setting up the request: ${error.message}`);
+        }
+      });
+  };
+
+  return controller;
+};

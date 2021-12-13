2114a39823dfc77871d741ce9a0535489c048f12
Guilherme Vasco <guilhermevasco08@gmail.com>
2021-12-13 13:20:16 -0300

    Update floraDoBrasilController.js

--- TaxonHub/src/Backend/api/controllers/floraDoBrasilController.js
+++ TaxonHub/src/Backend/api/controllers/floraDoBrasilController.js
@@ -1,23 +1,31 @@
 const axios = require("axios")
 module.exports = () => {
-    const controller = {};
+	const controller = {};
 
-    axios.get("http://servicos.jbrj.gov.br/flora")
-    .then((res) => {
-        controller.floraDoBrasil = (req, res) => res.status(200).json(res);
-        return response.send(res.data);
-    })
-    .catch(error => {
-        if (error.response){
-          throw new Error(
-            `Website responde error with status ${error.response.status}`,
-          );
-        } else if (error.request){
-          throw new Error(`Website request error: ${error.request}`);
-        } else {
-          throw new Error(`Error is setting up the request: ${error.message}`);
-        }
-    });
-  
-    return controller;
+	controller.floraDoBrasil = async (req, res) => {
+		let dados = await buscaDireta();
+
+		res.send(dados)
+	}
+	const buscaDireta = async () => {
+		return axios
+			.get("http://servicos.jbrj.gov.br/flora/", {
+			})
+			.then((response) => {
+				return response.data;
+			})
+			.catch((error) => {
+				if (error.response) {
+					throw new Error(
+					`Website retornou com erro: ${error.response.status}`
+				);
+				} else if (error.request) {
+					throw new Error(`Website request error: ${error.request}`);
+				} else {
+					throw new Error(`Error in setting up the request: ${error.message}`);
+				}
+			});
+	};
+
+	return controller;
 };
